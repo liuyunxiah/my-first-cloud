0125306a59dfdf4616140fe406e17e0f6732fb7f
liuyunxia <2216475641@qq.com>
2021-01-12 22:17:19 +0800

    eureka

--- Eureka-server/src/main/java/com/cloud/eureka/EruekaApplication.java
+++ Eureka-server/src/main/java/com/cloud/eureka/EruekaApplication.java
@@ -1,15 +1,14 @@
-package com.cloud.consumer;
+package com.cloud.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-@EnableEurekaClient
-public class OrderApplication {
-
+@EnableEurekaServer  //标志Eureka Service 端
+public class EruekaApplication {
     public static void main(String[] args) {
-        SpringApplication.run(OrderApplication.class, args);
+        SpringApplication.run(EruekaApplication.class, args);
     }
 }
