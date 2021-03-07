7a139f7e5a6d74d46bcff72745884ac682d7e512
liuyunxia <2216475641@qq.com>
2021-03-07 20:22:17 +0800

    nacos group namespaces

--- config-nacos/src/main/java/com/cloud/nacos/NacosConfigApplication.java
+++ config-nacos/src/main/java/com/cloud/nacos/NacosConfigApplication.java
@@ -3,10 +3,11 @@ package com.cloud.nacos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import java.util.concurrent.TimeUnit;
-
+@EnableDiscoveryClient
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class NacosConfigApplication {
 
--- config-nacos/src/main/java/com/cloud/nacos/controller/ConfigController.java
+++ config-nacos/src/main/java/com/cloud/nacos/controller/ConfigController.java
@@ -1,7 +1,10 @@
 package com.cloud.nacos.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,12 +14,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 public class ConfigController {
 
-    @Value("${current.env}")
-    private String env;
+//    @Value("${current.env}")
+//    private String env;
+
+    //1.
+    @Autowired
+    private ConfigurableApplicationContext applicationContext;
 
     @RequestMapping("/get")
     public String get() {
-        return env;
+        return applicationContext.getEnvironment().getProperty("current.env");
+    }
+
+    //2.分组DEAFAULT_GROUP
+    @Value("${config.info}")
+    private String info;
+
+    @GetMapping("/config/info")
+    public 	String nacosConfig() {
+        return info;
     }
 
 }
