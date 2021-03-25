977aafcdf0e9d38f7c8c85846dd00f8db171be38
liuyunxia <2216475641@qq.com>
2021-03-25 10:11:19 +0800

    pom文件-web修改

--- pay-nacos8001/src/main/java/com/cloud/nacos/controller/PaymentController.java
+++ pay-nacos8001/src/main/java/com/cloud/nacos/controller/PaymentController.java
@@ -3,10 +3,7 @@ package com.cloud.nacos.controller;
 import com.cloud.nacos.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/payment/nacos")
@@ -18,7 +15,13 @@ public class PaymentController {
 
     @GetMapping("/selectPaymentById/{id}")
     public String selectPaymentById_1(@PathVariable String id){
+        //http://localhost:8001/payment/nacos/selectPaymentById/1
         return paymentService.selectPaymentById(id);
+    }
+
+    @PostMapping("/selectPaymentById")
+    public String selectPaymentById_1(){
+        return "成功";
     }
 
 }
